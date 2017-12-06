20e0638ac6a169ec6cc35dc41afd9014e0da7b24
Edgar Mendes <EdgarSeca@ua.pt>
2017-12-06 09:59:36 +0000

    added folder src and changed gitignore

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/**
+ *       @file  main.c
+ *      @brief  Breve Descrição
+ *
+ * 	main.c - ficheiro onde esta a função main, da qual todas as outras funções são chamadas
+ *
+ *     @author  Edgar Mendes, EdgarSeca@ua.pt
+ *
+ *   @internal
+ *     Created  14-Out-2017
+ *     Company  University of Aveiro
+ *   Copyright  Copyright (c) 2017, Edgar Mendes
+ *
+ * =====================================================================================
+ */
+ 
+#define _MAIN_C_
+
+
+#include "myf.h"
+
+
+int main(int argc, char *argv[])
+{ 
+
+#if 1
+		
+	
+        return 0;
+        
+#endif
+}
